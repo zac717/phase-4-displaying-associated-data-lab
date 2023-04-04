c2ff8d78ed74c889036159ab9b82e1f6585b8086
Isaac Ongera <isaacongera717@gmail.com>
2023-04-04 17:03:07 +0300

    completed assignment

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,10 @@
 class ItemsController < ApplicationController
+  # GET /items
+  def index
+      items = Item.all
+      render json: items, include: :user
+  end
+
 end
+# reviews = Review.all.order(rating: :desc)
+# render json: reviews, include: :dog_house
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,14 @@
 class UsersController < ApplicationController
+  # GET /users/id
+  def show
+      user = User.find(params[:id])
+      render json: user, include: :items
+  end
+
+  private
+
+  def render_not_found_response
+      render json: { error: "Dog house not found" }, status: :not_found
+  end
+
 end
